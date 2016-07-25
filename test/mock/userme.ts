eaf218febec270108d3fb05e324975c7684535b4
Sune Bear <hi@sunebear.com>
2016-07-25 11:00:48 +0800

    ISSUE(schema/userme): Updated the UserMe Schema Definitions

--- test/mock/userme.ts
+++ test/mock/userme.ts
@@ -1,18 +1,26 @@
 export const userMe = {
-  _id: '54cb6200d1b4c6af47abe570',
-  email: 'yinan@teambition.com',
-  name: '龙逸楠',
-  avatarUrl: 'https://striker.teambition.net/thumbnail/110cb6197496463733d6d1d048675e0b5019/w/200/h/200',
-  created: '2015-01-30T10:50:40.258Z',
-  title: '',
-  birthday: null,
-  location: '',
+  _id: '55c02018fd0360a44c93ff97',
+  email: '123@123.com',
+  name: '宝宝摔倒了',
+  avatarUrl: 'https://striker.teambition.net/thumbnail/110g422c61261bc58ef11056140d99a01aab/w/200/h/200',
+  created: '2015-08-04T02:14:48.152Z',
+  title: '哎呦喂',
+  birthday: '2005-02-05T06:10:06.264Z',
+  location: 'asdfasdfasdf',
   phone: '',
-  isActive: true,
-  website: '',
-  pinyin: 'longyinan',
-  py: 'lyn',
+  isActive: false,
+  website: 'asdadsfadf',
+  pinyin: 'baobaoshuaidaoliao',
+  py: 'bbsdl',
   isNew: false,
+  plan: {
+    status: 'paid',
+    expired: '2030-11-06T02:26:43.497Z',
+    paidCount: 9,
+    membersCount: 10,
+    days: 5218,
+    objectType: 'user'
+  },
   notification: {
     comment: {
       mobile: true,
@@ -35,64 +43,59 @@ export const userMe = {
       email: false
     },
     involve: {
-     mobile: true,
-     email: false
+      mobile: true,
+      email: false
     },
     update: {
       mobile: true,
       email: false
     },
     daily: {
-      email: false
+      email: true
     },
     monthly: {
-      email: false
+      email: true
     }
+  },
+  lastEntered: {
+    web: '2016-07-25T02:11:51.745Z',
+    third: null
   },
   aliens: [
     {
-      _id: '560a43daf0f5f1ca619ddabd',
-      showname: 'Brooooooklyn',
+      _id: '569cc145443d43a60a1c3fef',
+      showname: 'guan-wy',
       refer: 'github'
     },
     {
-      _id: '560a43ebf0f5f1ca619ddad1',
-      showname: '太狼丶',
-      refer: 'weibo'
-    },
-    {
-      _id: '5625ba64f0f5f1ca61ab9146',
-      refer: 'evernote'
-    },
-    {
-      _id: '56397500543ec27237d62400',
-      showname: '太狼',
-      refer: 'open_wx_service'
+      _id: '5773629a841059b6b09f7292',
+      showname: '',
+      refer: 'evernote_international'
     }
   ],
-  strikerAuth: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiI1NGNiNjIwMGQxYjRjNmFmNDdhYmU1NzAiLCJleHAiOjE0NTE0NTE2MTEsInN0b3JhZ2UiOiJzdHJpa2VyLWh6In0.td1njGW4_CD2G3bii6r0bBUD3hJMz7A6F7uBsSaRwpI',
+  strikerAuth: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiI1NWMwMjAxOGZkMDM2MGE0NGM5M2ZmOTciLCJleHAiOjE0Njk0OTkxMTEsInN0b3JhZ2UiOiJkZWZhdWx0In0.dAOHF43SqgGuJqCAphlcmPyUv-i29XK5G4DlR7dgtlE',
   phoneForLogin: '',
   enabledGoogleTwoFactor: false,
   emails: [
     {
-      email: 'yinan@teambition.com',
-      state: 1,
-      _id: '54cb6200d1b4c6af47abe571',
-      id: '54cb6200d1b4c6af47abe571'
+      email: '123@123.com',
+      state: 0,
+      _id: '55c02018fd0360a44c93ff98',
+      id: '55c02018fd0360a44c93ff98'
     }
   ],
-  snapperToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiI1NGNiNjIwMGQxYjRjNmFmNDdhYmU1NzAiLCJleHAiOjE0NTE1MzgwMTF9.h8SDsmHTewjbGDsxT5vg60A4wOImheoG-vkmBcmq9wA',
+  snapperToken: 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0Njk1ODU1MTEsInVzZXJJZCI6IjU1YzAyMDE4ZmQwMzYwYTQ0YzkzZmY5NyIsInNvdXJjZSI6InRlYW1iaXRpb24ifQ.9a05JWVUJnL0PUW53k_gKo7utg2T762R529rrQR82Ps',
   badge: 0,
-  inbox: 0,
   normal: 0,
   ated: 0,
   later: 0,
   private: 0,
+  inbox: 0,
   hasNormal: false,
   hasAted: false,
   hasLater: false,
   hasPrivate: false,
-  calLink: 'webcal://www.teambition.com/api/events/e77cef1b5a2832b597d3ee614bc1598cad00e837074aa0674b7859df0ccfc642.ics',
-  taskCalLink: 'webcal://www.teambition.com/api/tasks/9e805b181c004ee47cbf7bbc5d8d8d569dd8352f38a7367bedc1c9497904ee1a.ics',
-  joinedProjectsCount: 48
+  calLink: 'webcal://project.ci/api/events/972cb4b813b1c13a847df2f29435f41517173df981882ce1bf75c33778ab03bb.ics',
+  taskCalLink: 'webcal://project.ci/api/tasks/8bfe94edbe1d4f853c0cc97c91c07e99cdb83e89f54ff365016169e5b985c83b.ics',
+  joinedProjectsCount: 69
 }
