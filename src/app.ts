2f403f5567b49bf3ea4b53f58d9e809f8f4c402e
LongYinan <lynweklm@gmail.com>
2016-09-12 17:56:54 +0800

    0.4.0-alpha.5

--- src/app.ts
+++ src/app.ts
@@ -65,7 +65,7 @@ const teambition = ctx['teambition']
 
 const sdk = teambition.sdk = Object.create(null)
 
-sdk.version = '0.4.0-alpha.4'
+sdk.version = '0.4.0-alpha.5'
 
 sdk.socket = new Client()
 
