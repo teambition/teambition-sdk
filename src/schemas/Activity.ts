3e194c62931379ebe3af84c10b2c564c7bbd4c38
LongYinan <lynweklm@gmail.com>
2016-09-13 09:53:59 +0800

    hotfix: 修复了 ActiviySchema 定义的错误

--- src/schemas/Activity.ts
+++ src/schemas/Activity.ts
@@ -22,74 +22,73 @@ export interface Locales {
 }
 
 export interface ActivityData extends ISchema {
+  _boundToObjectId: string
+  _creatorId: string
   _id: string
   action: string
-  rawAction: string
-  created: number
   boundToObjectType: string
-  _boundToObjectId: string
-  creator: ExecutorOrCreator
-  title: string
   content: {
     comment?: string
-    attachments: File[]
-    mentionsArray: string[]
-    mentions: {
+    attachments?: File[]
+    voice?: string[]
+    mentionsArray?: string[]
+    mentions?: {
       [index: string]: string
     }
-    attachmentsName: string
-    creator: string
+    attachmentsName?: string
+    creator?: string
     linked?: {
       _id: string
       _projectId: string
       _objectId: string
       objectType: string
       title: string
+      url: string
     }
   }
-  isComment: boolean
-  icon: string
-  creatorName: string
-  creatorAvatar: string
-  comment: string
-  linked: {
+  created: number
+  creator: ExecutorOrCreator
+  rawAction: string
+  rootId: string
+  title: string
+  isComment?: boolean
+  icon?: string
+  creatorName?: string
+  creatorAvatar?: string
+  comment?: string
+  linked?: {
     _id?: string
   }
-  locales: Locales
+  locales?: Locales
 }
 
 @schemaName('Activity')
 export default class Activity extends Schema<ActivityData> implements ActivityData {
+  _boundToObjectId: string = undefined
+  _creatorId: string = undefined
   _id: string = undefined
   action: string = undefined
-  rawAction: string = undefined
-  created: number = undefined
   boundToObjectType: string = undefined
-  _boundToObjectId: string = undefined
-  creator: ExecutorOrCreator = undefined
-  title: string = undefined
   content: {
     comment?: string
-    attachments: File[]
-    mentionsArray: string[]
-    mentions: {[index: string]: string}
-    attachmentsName: string
-    creator: string
+    attachments?: File[]
+    mentionsArray?: string[]
+    voice?: string[]
+    mentions?: {[index: string]: string}
+    attachmentsName?: string
+    creator?: string
     linked?: {
       _id: string
       _projectId: string
       _objectId: string
       objectType: string
       title: string
+      url: string
     }
   } = undefined
-  isComment: boolean = undefined
-  icon: string = undefined
-  creatorName: string = undefined
-  creatorAvatar: string = undefined
-  comment: string = undefined
-  linked: {
-    _id?: string
-  } = undefined
-  locales: Locales = undefined
+  created: number = undefined
+  creator: ExecutorOrCreator = undefined
+  rawAction: string = undefined
+  rootId: string = undefined
+  title: string = undefined
 }
