ffeab8c9a0b68308ec2d7d6a2471aacad4745672
LongYinan <lynweklm@gmail.com>
2016-10-11 15:45:01 +0800

    chore: 使用 @types，去掉 typings 依赖
    TPLN-305

--- test/unit/type.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../typings/globals/mocha/index.d.ts" />
-/// <reference path="../../typings/globals/node/index.d.ts" />
-/// <reference path="../../typings/globals/chai/index.d.ts" />
-/// <reference path="../../typings/globals/sinon-chai/index.d.ts" />
-/// <reference path="../../typings/globals/sinon/index.d.ts" />
-/// <reference path="../../typings/globals/whatwg-streams/index.d.ts" />
-/// <reference path="../../typings/globals/whatwg-fetch/index.d.ts" />
