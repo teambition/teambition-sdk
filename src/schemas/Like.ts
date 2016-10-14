f4823d9bb2159e5296aebbac219a3df976dfcc00
LongYinan <lynweklm@gmail.com>
2016-10-14 10:46:25 +0800

    fix(like): 废弃 likesCount 属性

--- src/schemas/Like.ts
+++ src/schemas/Like.ts
@@ -6,7 +6,6 @@ export interface LikeData extends ISchema {
   _id: string
   _boundToObjectId: string
   _boundToObjectType: string
-  likesCount: number
   likesGroup: ExecutorOrCreator[]
 }
 
@@ -15,6 +14,5 @@ export default class LikeSchema extends Schema<LikeData> implements LikeData {
   _id: string = undefined
   @bloodyParentWithProperty('_boundToObjectType') _boundToObjectId: string = undefined
   _boundToObjectType: string = undefined
-  likesCount: number = undefined
   likesGroup: ExecutorOrCreator[] = undefined
 }
