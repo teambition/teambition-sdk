31124db4801099cbc18399114eda4ca2cedd2817
LongYinan <lynweklm@gmail.com>
2016-11-23 14:52:07 +0800

    0.6.3

--- src/app.ts
+++ src/app.ts
@@ -58,12 +58,12 @@ declare const global: any
 const ctx = typeof global === 'undefined' ? window : global
 
 export interface SDK {
-  readonly version: '0.6.2'
+  readonly version: '0.6.3'
   readonly socket: Client
 }
 
 const sdk = {
-  version: '0.6.2',
+  version: '0.6.3',
   socket: new Client()
 }
 
