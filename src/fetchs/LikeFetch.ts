fdcc9213a757c08a9e0ac3e55a19362413247bef
LongYinan <lynweklm@gmail.com>
2016-12-05 11:26:50 +0800

    fix(LikeFetch): 修复 LikeFetch 对 entries 处理不正确的情况

--- src/fetchs/LikeFetch.ts
+++ src/fetchs/LikeFetch.ts
@@ -7,15 +7,18 @@ import { DetailObjectType, DetailObjectId } from '../teambition'
 export class LikeFetch extends BaseFetch {
 
   get(objectType: DetailObjectType, objectId: DetailObjectId): Observable<LikeData> {
-    return this.fetch.get(`${objectType}s/${objectId}/like`, { all: '1'})
+    const fetchNamespace = objectType !== 'entry' ? `${objectType}s` : 'entries'
+    return this.fetch.get(`${fetchNamespace}/${objectId}/like`, { all: '1'})
   }
 
   like(objectType: DetailObjectType, objectId: DetailObjectId): Observable<LikeData> {
-    return this.fetch.post(`${objectType}s/${objectId}/like`)
+    const fetchNamespace = objectType !== 'entry' ? `${objectType}s` : 'entries'
+    return this.fetch.post(`${fetchNamespace}/${objectId}/like`)
   }
 
   unlike(objectType: DetailObjectType, objectId: DetailObjectId): Observable<LikeData> {
-    return this.fetch.delete(`${objectType}s/${objectId}/like`)
+    const fetchNamespace = objectType !== 'entry' ? `${objectType}s` : 'entries'
+    return this.fetch.delete(`${fetchNamespace}/${objectId}/like`)
   }
 }
 
