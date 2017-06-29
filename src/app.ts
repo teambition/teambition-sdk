69176e6a2719d4d6b0f20b6706bfa09eda3ee0a3
Saviio <jiaqi@teambition.com>
2017-06-29 19:41:10 +0800

    0.6.10

--- src/app.ts
+++ src/app.ts
@@ -63,12 +63,12 @@ declare const global: any
 const ctx = typeof global === 'undefined' ? window : global
 
 export interface SDK {
-  readonly version: '0.6.9'
+  readonly version: '0.6.10'
   readonly socket: Client
 }
 
 const sdk = {
-  version: '0.6.9',
+  version: '0.6.10',
   socket: new Client()
 }
 
