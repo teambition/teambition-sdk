6dd736dea2434c69ac9847edc2200d7cb037818f
chuan6 <chuan6.dev@gmail.com>
2017-09-04 18:20:46 +0800

    fix(schemas/member): 添加 ProjectMember 和 OrganizationMember 表
    ...以暂时解决 Member 数据中 _boundToObjectType 字段遭到互相覆盖的问题，
    影响上层查询的可靠性。
    
    这两张表基于现有的 Member schema。

--- src/schemas/Project.ts
+++ src/schemas/Project.ts
@@ -114,9 +114,9 @@ const Schema: SchemaDef<ProjectSchema> = {
   creator: {
     type: Relationship.oneToOne,
     virtual: {
-      name: 'User',
-      where: (userTable: any) => ({
-        _creatorId: userTable._id
+      name: 'ProjectMember',
+      where: (memberTable: any) => ({
+        _creatorId: memberTable._id
       })
     }
   },
