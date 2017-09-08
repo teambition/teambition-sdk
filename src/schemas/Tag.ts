330af0bda5e0b11f2697241e249c237b8383b3ab
Kombu <aicest@users.noreply.github.com>
2017-09-08 11:21:25 +0800

    fix(schema/Tag): 导出 TagSchema

--- src/schemas/Tag.ts
+++ src/schemas/Tag.ts
@@ -1,4 +1,7 @@
+import { RDBType } from 'reactivedb/interface'
+import { SchemaDef } from 'reactivedb/interface'
 import { TagId, UserId, ProjectId, DefaultColors } from 'teambition-types'
+import { schemas } from '../SDK'
 
 export interface TagSchema {
   _creatorId: UserId
@@ -14,3 +17,45 @@ export interface TagSchema {
   eventsCount?: number
   worksCount?: number
 }
+
+const schema: SchemaDef<TagSchema> = {
+  _id: {
+    type: RDBType.STRING,
+    primaryKey: true,
+  },
+  _creatorId: {
+    type: RDBType.STRING,
+  },
+  _projectId: {
+    type: RDBType.STRING,
+  },
+  color: {
+    type: RDBType.STRING,
+  },
+  created: {
+    type: RDBType.DATE_TIME,
+  },
+  updated: {
+    type: RDBType.DATE_TIME,
+  },
+  isArchived: {
+    type: RDBType.BOOLEAN,
+  },
+  name: {
+    type: RDBType.STRING,
+  },
+  postsCount: {
+    type: RDBType.NUMBER
+  },
+  tasksCount: {
+    type: RDBType.NUMBER
+  },
+  eventsCount: {
+    type: RDBType.NUMBER
+  },
+  worksCount: {
+    type: RDBType.NUMBER
+  },
+}
+
+schemas.push({ name: 'Tag', schema })
