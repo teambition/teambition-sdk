a5d03ad83309ba9a3230c84ee2153a963a7a6786
chuan6 <chuan6.dev@gmail.com>
2017-10-19 07:38:39 +0800

    feat(event/isAllDay): 为日程添加 isAllDay 属性
    ...及相关属性：allDayStart, allDayEnd。
    
    更新 EventSchema。
    
    更新日程工具函数中 isAllDay 的逻辑，优先通过日程模型上的 isAllDay 属性
    判断，仅当日程模型上找不到有布尔值的 isAllDay 属性，使用原逻辑通过开始
    时间和结束时间来判断。并添加相关测试。
    
    添加为全天日程生成当地时区遵循原全天判断规则的startDate/endDate的函数

---
+++ src/apis/event/utils/all-day.ts
@@ -0,0 +1,38 @@
+import { EventSchema } from '../../../schemas/Event'
+
+const dateStrLen = 'YYYY-MM-DD'.length
+
+type AllDayInfo = {
+  isAllDay: boolean,
+  allDayStart: string,
+  allDayEnd: string
+}
+
+export const getAllDayInfo = (e: Readonly<EventSchema>): AllDayInfo | null => {
+  const { isAllDay, allDayStart, allDayEnd } = e
+
+  if (e.hasOwnProperty('isAllDay') &&
+      e.hasOwnProperty('allDayStart') &&
+      e.hasOwnProperty('allDayEnd')
+  ) {
+    return { isAllDay, allDayStart, allDayEnd }
+  } else {
+    return null
+  }
+}
+
+export const generateStartEndDate = (info: AllDayInfo): { startDate: string, endDate: string } => {
+  const start = new Date(info.allDayStart.slice(0, dateStrLen))
+  const end = new Date(info.allDayEnd.slice(0, dateStrLen))
+
+  // 获得当前时区的具体开始时间和结束时间，兼容 legacy 模式的全天日程定义。
+  const startDate = new Date(start.getFullYear(), start.getMonth(), start.getDate())
+  const endDate = new Date(end.getFullYear(), end.getMonth(), end.getDate())
+
+  return {
+    startDate: startDate.toISOString(),
+    endDate: endDate.toISOString()
+  }
+}
+
+export const isAllDay = (info: AllDayInfo): boolean => info.isAllDay
--- src/apis/event/utils/index.ts
+++ src/apis/event/utils/index.ts
@@ -1,11 +1,14 @@
-import { EventSchema } from '../../schemas/Event'
+import { EventSchema } from '../../../schemas/Event'
 import { EventId } from 'teambition-types'
+import { SDKLogger } from '../../../utils/Logger'
+
+import * as ad from './all-day'
 
 /**
  * 判断一个日程对象是否为重复日程。
  * 注意：有重复规则，但仅能推导得零个可用时间点的日程，会返回 true。
  */
-export const isRecurrent = (event: EventSchema) =>
+export const isRecurrent = (event: Readonly<EventSchema>) =>
   !!event.recurrence && event.recurrence.length > 0
 
 const msPerDay = 24 * 60 * 60 * 1000
@@ -15,11 +18,33 @@ const epochTime = new Date(1970, 0, 1).valueOf()
 
 /**
  * 判断一个日程是否为全天日程。
- * 目前全天日程的定义为，开始时间为零点，结束时间为第二天零点，或
+ */
+export const isAllDay = (e: Readonly<EventSchema>): boolean => {
+  const snippet = ad.getAllDayInfo(e)
+  let currentResult: boolean
+  const legacyResult = isAllDayLegacy(e)
+
+  if (snippet) {
+    currentResult = ad.isAllDay(snippet)
+
+    if (currentResult !== legacyResult) { // for debugging purpose
+      SDKLogger.warn('isAllDay migration incompatibility:', {
+        current: e.isAllDay,
+        legacy: legacyResult
+      })
+    }
+    return currentResult
+  } else {
+    return legacyResult
+  }
+}
+
+/**
+ * LEGACY 全天日程的定义：开始时间为零点，结束时间为第二天零点，或
  * 接下来第 n 天零点，的日程。
  * 注意：零点判断根据当地时区得。
  */
-export const isAllDay = (e: EventSchema): boolean => {
+const isAllDayLegacy = (e: Readonly<EventSchema>): boolean => {
   const startTime = new Date(e.startDate).valueOf()
 
   if ((startTime - epochTime) % msPerDay !== 0) {
@@ -29,6 +54,16 @@ export const isAllDay = (e: EventSchema): boolean => {
   const duration = new Date(e.endDate).valueOf() - startTime
 
   return duration > 0 && duration % msPerDay === 0
+}
+
+export const allDayEventStartEndDate = (e: Readonly<EventSchema>) => {
+  const snippet = ad.getAllDayInfo(e)
+
+  if (!snippet) { // pass through
+    return { startDate: e.startDate, endDate: e.endDate }
+  }
+
+  return ad.generateStartEndDate(snippet)
 }
 
 /**
