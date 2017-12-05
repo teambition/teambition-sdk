6d53b17b50fa89da4c6f0b999b1906d3080754b8
chuan6 <chuan6.dev@gmail.com>
2017-12-05 14:47:40 +0800

    test(sockets/TableAlias): 为已有的别名匹配条目添加测试
    ...保证在未来代码行为改变时，有失败的测试作为提醒。

--- src/sockets/TableAlias.ts
+++ src/sockets/TableAlias.ts
@@ -1,7 +1,9 @@
 /**
  * 当 websocket 消息中类型字段的内容与目标表名不符，
- * 请在这里添加转换规则。目标表名对应实际表名，注意查阅 schemas/
- * 下的表名定义。
+ * 请在这里添加转换规则。注意：
+ * 1. 此处 key 不做单复数转换，如有单复数形式，请同时添加，如：
+ *    'ChatMessage' 和 'ChatMessages'。
+ * 2. 目标表名对应实际表名，注意查阅 schemas/ 下的表名定义。
  *
  * 注：大小写区别，及单复数形式在名字末尾仅相差一个 's' 的情况
  * 已做自动转换，无需在此处添加映射。
@@ -14,3 +16,5 @@ export default {
   Activities: 'Activity',
   HomeActivities: 'Activity'
 }
+// 如添加条目，请配合相关项目调试，并在
+// test/sockets/mapToTable.spec.ts 添加相应测试。
