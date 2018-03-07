8ac3ce3f9bb0c959013112862515e1cce5e3ed6d
Fai <aicest@users.noreply.github.com>
2018-03-07 14:53:28 +0800

    feat(apis/scenariofieldconfig): 添加 `getScenarioFieldConfigs` 接口

---
+++ test/fixtures/scenariofieldconfigs.fixture.ts
@@ -0,0 +1,109 @@
+export const taskScenariofieldConfig = {
+  _id: '59ef11e0d564c422680d73e0',
+  name: '任务',
+  _creatorId: '56efaff50e5a879506e2dbb4',
+  objectType: 'task',
+  _projectId: '59ef11dfb29c94454cd453d7',
+  _taskflowId: null,
+  icon: 'order',
+  proTemplateConfigType: null,
+  scenariofields: [
+    {
+      fieldType: 'customfield',
+      _customfieldId: '5a2514d9f270996e5f3879f6',
+      _id: '5a279324aa22f916d5a12ce2',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'tag',
+      _id: '5a279324aa22f916d5a12ce1',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'customfield',
+      _customfieldId: '5a1686eef3758f5bf0f11dec',
+      _id: '5a279324aa22f916d5a12ce0',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'customfield',
+      _customfieldId: '5a168802f3758f5bf0f11e2d',
+      _id: '5a279324aa22f916d5a12cdf',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'customfield',
+      _customfieldId: '5a168436c17f605c00c7a3f4',
+      _id: '5a279324aa22f916d5a12cde',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'customfield',
+      _customfieldId: '5a168371f3758f5bf0f11ce2',
+      _id: '5a279324aa22f916d5a12cdd',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'customfield',
+      _customfieldId: '5a168024c17f605c00c7a3b9',
+      _id: '5a279324aa22f916d5a12cdc',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    }
+  ],
+  created: '2017-10-24T10:11:44.483Z',
+  updated: '2017-12-19T07:59:26.348Z',
+  isDefault: true,
+  displayed: true
+}
+
+export const eventScenariofieldConfig = {
+  _id: '59fa8b97d564c422688da661',
+  name: '日程',
+  _creatorId: '56efaff50e5a879506e2dbb4',
+  objectType: 'event',
+  _projectId: '59ef11dfb29c94454cd453d7',
+  _taskflowId: null,
+  icon: 'event',
+  proTemplateConfigType: null,
+  scenariofields: [
+    {
+      fieldType: 'location',
+      _id: '59fa8b9774d33ffedc74f12f',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'content',
+      _id: '59fa8b9774d33ffedc74f12e',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    },
+    {
+      fieldType: 'tag',
+      _id: '59fa8b9774d33ffedc74f12d',
+      required: false,
+      displayed: true,
+      _roleIds: []
+    }
+  ],
+  created: '2017-11-02T03:05:59.121Z',
+  updated: '2017-11-02T03:05:59.121Z',
+  isDefault: true,
+  displayed: false
+}
