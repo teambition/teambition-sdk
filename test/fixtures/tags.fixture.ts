e0ae36ee98ff8342c5d73624de406f3cc82af0c3
Fai <aicest@users.noreply.github.com>
2018-03-13 14:34:57 +0800

    feat(apis/tag): 添加 `getTags()` 接口

---
+++ test/fixtures/tags.fixture.ts
@@ -0,0 +1,10 @@
+export const tag = {
+  _id: '59b1154450ad9479a1b6ff58',
+  _projectId: '59a50fdcd1af1e04feb7ae9d',
+  name: 'ww',
+  _creatorId: '56efaff50e5a879506e2dbb4',
+  updated: '2017-09-07T09:45:40.840Z',
+  created: '2017-09-07T09:45:40.840Z',
+  isArchived: null,
+  color: 'blue'
+}
