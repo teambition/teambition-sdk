53ed508a2e2a669b9d44f1b9beeb902447e375ea
Fai <aicest@users.noreply.github.com>
2018-03-13 14:30:49 +0800

    feat(apis/customfieldlink): 添加 `getCustomFieldLinks()` 接口

---
+++ src/apis/customfieldlink/get.ts
@@ -0,0 +1,48 @@
+import { Observable } from 'rxjs/Observable'
+import { QueryToken } from 'reactivedb'
+
+import { CacheStrategy } from '../../Net'
+import { CustomFieldLinkSchema } from '../../schemas'
+import { ProjectId, CustomFieldBoundType } from 'teambition-types'
+import { SDK } from '../../SDK'
+import { SDKFetch } from '../../SDKFetch'
+
+export function getCustomFieldLinksFetch(
+  this: SDKFetch,
+  projectId: ProjectId,
+  boundType: CustomFieldBoundType
+): Observable<CustomFieldLinkSchema> {
+  return this.get<CustomFieldLinkSchema>(
+    `projects/${projectId}/customfieldlinks`,
+    { boundType }
+  )
+}
+
+declare module '../../SDKFetch' {
+  interface SDKFetch {
+    getCustomFieldLinks: typeof getCustomFieldLinksFetch
+  }
+}
+
+SDKFetch.prototype.getCustomFieldLinks = getCustomFieldLinksFetch
+
+export function getCustomFieldLinks(
+  this: SDK,
+  projectId: ProjectId,
+  boundType: CustomFieldBoundType
+): QueryToken<CustomFieldLinkSchema> {
+  return this.lift<CustomFieldLinkSchema>({
+    cacheValidate: CacheStrategy.Request,
+    tableName: 'CustomFieldLink',
+    request: this.fetch.getCustomFieldLinks(projectId, boundType),
+    query: { where: [{ _projectId: projectId }] }
+  })
+}
+
+declare module '../../SDK' {
+  interface SDK {
+    getCustomFieldLinks: typeof getCustomFieldLinks
+  }
+}
+
+SDK.prototype.getCustomFieldLinks = getCustomFieldLinks
