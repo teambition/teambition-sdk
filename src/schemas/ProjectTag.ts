bea5842f8fec9dbb2af2b74039444f88b51e90c3
chuan6 <chuan6.dev@gmail.com>
2018-04-12 17:46:15 +0800

    feat(schema): 更新 ProjectTagSchema

--- src/schemas/ProjectTag.ts
+++ src/schemas/ProjectTag.ts
@@ -1,8 +1,17 @@
-import { OrganizationId, ProjectTagId } from 'teambition-types'
+import { OrganizationId, ProjectId, ProjectTagId, UserId } from 'teambition-types'
 
 export interface ProjectTagSchema {
   _id: ProjectTagId
   _organizationId: OrganizationId
   name: string
   pos: number
+  projectIds: ProjectId[] // 该分组下的项目的 ids
+  childProjectIds: ProjectId[] // 该分组子分组（递归）下的项目的 ids
+  _creatorId: UserId
+  isDeleted: boolean
+  ancestorIds: ProjectTagId[]
+  style: string
+  color: string
+  created: string
+  updated: string
 }
