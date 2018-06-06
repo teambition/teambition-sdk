c13bf566f81018f0878dbf06bbdd04b6da348dee
chuan6 <chuan6.dev@gmail.com>
2018-06-06 23:10:14 +0800

    refactor(typings): 改善和纠正各种 id 类型的性质
    各种 id 类型如：UserId, ProjectId, TaskId 等等，它们应该有下列性质：
    
     1.可以赋给 string 类型（允许如：`const x: string = id as UserId`）
     2.相互之间不可赋值（如遇 `const tid: TeamId = uid as UserId` 会报错，
       需要手动强转）
     3.string 不可以赋给它们（如遇 `const id: UserId = 'hello'` 会报错，需
       要手动强转）
    
    原来 `interface X extends String { kind?: 'X' }` 的实现，满足了2，但没
    有满足1、3。
    
    不满足1，导致当需要将 id 数据从带有场景上下文的业务代码传给不关心业务逻
    辑而只是简单接受 string 的底层组件时，需要通过 `as string` 强转，如果
    该信息包在一个对象结构里，那这个对象结构要么需要 `as any`，结果丢失所
    有类型信息，要么底层组件的对应对象结构类型声明就需要添加类型参数（泛型
    声明），结果增加冗长而意义不大的泛型声明。
    
    而不满足3，会漏掉很多类型检查，因为并不是任何 string 类型的值都可以赋
    值给特定 id 类型的。
    
    新的写法是：
    
      `type X = string & { kind: 'X' }`
    
    它能同时满足1、2、3。
    
    参考：
    
     - https://codemix.com/opaque-types-in-javascript/
     - https://github.com/Microsoft/TypeScript/issues/15807
     - https://github.com/Microsoft/TypeScript/issues/4895
     - https://github.com/Microsoft/TypeScript/issues/202
     - https://github.com/Microsoft/TypeScript/blob/d9b93903c035e48c8da1d731332787f83efc4619/src/compiler/types.ts#L54

--- test/apis/like.spec.ts
+++ test/apis/like.spec.ts
@@ -4,10 +4,13 @@ import { describe, it, beforeEach, afterEach } from 'tman'
 import { createSdk, SDK, LikeSchema } from '../index'
 import like from '../fixtures/like.fixture'
 import { mock, restore } from '../utils'
+import { DetailObjectId } from 'teambition-types'
 
 describe('LikeApi request spec: ', () => {
   let sdk: SDK
   let mockResponse: <T>(m: T, delay?: number | Promise<any>) => void
+  const mockTaskId = 'mocktask' as DetailObjectId
+  const mockTaskLikeId = 'mocktask:like'
 
   beforeEach(() => {
     sdk = createSdk()
@@ -21,7 +24,7 @@ describe('LikeApi request spec: ', () => {
   it('get like should pass', function* () {
     mockResponse(like)
 
-    yield sdk.getLike('task', 'mocktask')
+    yield sdk.getLike('task', mockTaskId)
       .values()
       .do(([r]) => {
         delete r._id
@@ -32,15 +35,15 @@ describe('LikeApi request spec: ', () => {
   it('toggle like should pass', function* () {
     yield sdk.database.insert('Like', {
       ...like,
-      _id: 'mocktask:like'
+      _id: mockTaskLikeId
     })
 
     mockResponse({ ...like, isLike: false })
 
-    yield sdk.toggleLike('task', 'mocktask', true)
+    yield sdk.toggleLike('task', mockTaskId, true)
 
     yield sdk.database.get<LikeSchema>('Like', {
-      where: { _id: 'mocktask:like' }
+      where: { _id: mockTaskLikeId }
     })
       .values()
       .do(([r]) => {
@@ -49,10 +52,10 @@ describe('LikeApi request spec: ', () => {
 
     mockResponse({ ...like, isLike: true })
 
-    yield sdk.toggleLike('task', 'mocktask', false)
+    yield sdk.toggleLike('task', mockTaskId, false)
 
     yield sdk.database.get<LikeSchema>('Like', {
-      where: { _id: 'mocktask:like' }
+      where: { _id: mockTaskLikeId }
     })
       .values()
       .do(([r]) => {
