f0cf1c406688964f98aa41e5180d3f7df6493291
chuan6 <chuan6.dev@gmail.com>
2018-06-06 15:33:26 +0800

    squash! 移除 SchemaDef 定义，因为目前没有使用缓存
    ...并选择只 export 出来 DashboardCardSchema，其他辅助类型暂不公开。

--- src/schemas/DashboardCard.ts
+++ src/schemas/DashboardCard.ts
@@ -1,9 +1,4 @@
-import { SchemaDef, RDBType } from 'reactivedb/interface'
-import { schemaColl } from './schemas'
-import {
-  DashboardCardId,
-  UserId
-} from 'teambition-types'
+import { DashboardCardId, UserId } from 'teambition-types'
 
 export enum OfficialKey {
   Recent = 'recent',
@@ -40,60 +35,11 @@ export interface DashboardCardSchema {
   dataSource: DataSource
   dataUrl: string
   description: string
-  thumbnailUrl?: string
   isDeleted: boolean
   name: string
   officialKey?: OfficialKey
   pos: number
   updated: string
   viewType: CardType
-  _userId: UserId
+  _userId: UserId // todo: 可能改名为 _creatorId
 }
-
-const Schema: SchemaDef<DashboardCardSchema> = {
-  _id: {
-    type: RDBType.STRING,
-    primaryKey: true
-  },
-  category: {
-    type: RDBType.STRING
-  },
-  created: {
-    type: RDBType.STRING
-  },
-  dataSource: {
-    type: RDBType.STRING
-  },
-  dataUrl: {
-    type: RDBType.STRING
-  },
-  description: {
-    type: RDBType.STRING
-  },
-  thumbnailUrl: {
-    type: RDBType.STRING
-  },
-  isDeleted: {
-    type: RDBType.BOOLEAN
-  },
-  name: {
-    type: RDBType.STRING
-  },
-  officialKey: {
-    type: RDBType.STRING
-  },
-  pos: {
-    type: RDBType.NUMBER
-  },
-  updated: {
-    type: RDBType.STRING
-  },
-  viewType: {
-    type: RDBType.STRING
-  },
-  _userId: {
-    type: RDBType.STRING
-  }
-}
-
-schemaColl.add({ schema: Schema, name: 'DashboardCard' })
