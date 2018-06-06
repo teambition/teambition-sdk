4b2cb6c9b8beeddf41a911d1b77db97b7849f50a
Guanwy <wenyan@teambition.com>
2018-06-06 14:37:51 +0800

    feat(Schema): 新增工作台卡片 DashboardCardSchema

---
+++ src/schemas/DashboardCard.ts
@@ -0,0 +1,45 @@
+import { DashboardCardId, UserId } from 'teambition-types'
+
+export enum DashboardOfficialKey {
+  Recent = 'recent',
+  TaskExecuted = 'task_executed',
+  TaskInvolved = 'task_involved',
+  TaskCreated = 'task_created',
+  Favorite = 'favorite',
+  WorkCreated = 'work_created',
+  EventInvolved = 'event_involved',
+  History = 'history',
+  Apps = 'apps'
+}
+
+export enum DashboardCardCategory {
+  Custom = 'custom',
+  Official = 'official'
+}
+
+export enum DashboardDataSource {
+  Tql = 'tql',
+  Api = 'api'
+}
+
+export enum DashboardCardType {
+  ListPlain = 'list.plain',
+  ListPage = 'list.page',
+  ListModal = 'list.modal'
+}
+
+export interface DashboardCardSchema {
+  _id: DashboardCardId
+  category: DashboardCardCategory
+  created: string
+  dataSource: DashboardDataSource
+  dataUrl: string
+  description: string
+  isDeleted: boolean
+  name: string
+  officialKey?: DashboardOfficialKey
+  pos: number
+  updated: string
+  viewType: DashboardCardType
+  _userId: UserId
+}
