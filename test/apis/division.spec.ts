f45d2555b8aea4532afd37fcb239fea660b160fb
Fai <aicest@users.noreply.github.com>
2018-11-12 16:12:31 +0800

    fixup! feat(schemas/Task): 添加 `divisions` 字段

--- test/apis/division.spec.ts
+++ test/apis/division.spec.ts
@@ -1,10 +1,40 @@
-import { describe, beforeEach, it } from 'tman'
+import { describe, beforeEach, it, afterEach } from 'tman'
 import { Scheduler } from 'rxjs'
 import { TaskDivisionType, TaskId } from 'teambition-types'
-import { SDK, createSdk, TaskSchema } from '..'
+import { SDK, createSdk, TaskSchema, SDKFetch } from '..'
 import { mock, expectToDeepEqualForFieldsOfTheExpected } from '../utils'
 
+const fetchMock = require('fetch-mock')
+
 describe('TaskDivision Request', () => {
+  let sdkFetch: SDKFetch
+
+  beforeEach(() => {
+    sdkFetch = new SDKFetch()
+    sdkFetch.setAPIHost('')
+  })
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  it('should update Task divisions', function*() {
+    const taskId = 'mock-task-id' as TaskId
+    const divisions = ['scenariofields'] as TaskDivisionType[]
+
+    const url = `/tasks/${taskId}/divisions`
+    fetchMock.once(url, { divisions })
+
+    yield sdkFetch
+      .updateTaskDivisions(taskId, divisions)
+      .subscribeOn(Scheduler.asap)
+      .do((resp) => {
+        expectToDeepEqualForFieldsOfTheExpected(resp, { divisions })
+      })
+  })
+})
+
+describe('TaskDivision', () => {
   let sdk: SDK
   let mockResponse: <T>(m: T, schedule?: number | Promise<any>) => void
 
