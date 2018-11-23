95937c5d0f0af21599514617b20d3832a644cfdc
Fai <aicest@users.noreply.github.com>
2018-11-23 21:19:52 +0800

    fixup! fix(schemas/ScenarioField): 补充 `customfield` 字段

--- src/apis/scenariofieldconfig/util.ts
+++
@@ -1,7 +0,0 @@
-import { ScenarioFieldSchema, CustomScenarioFieldSchema } from '../../schemas'
-
-export const isCustomScenarioField = (
-  it: ScenarioFieldSchema
-): it is CustomScenarioFieldSchema => {
-  return it.fieldType === 'customfield'
-}
