e8d7ade9114628f751dab0ab390970c4f19fde49
Fai <aicest@users.noreply.github.com>
2019-01-13 16:22:33 +0800

    fix(apis/scenariofieldconfig): 有关 GET 接口，支持 withCustomfields 参数
    - 有关 GET 接口，支持 `withCustomfields` 参数
    - 新增 `updateScenarioFieldConfig` 接口
    - 新增 `updateScenarioFieldConfigFields` 接口
    - 新增 `deleteOrgScenarioFieldConfig` 接口

--- src/apis/scenariofieldconfig/index.ts
+++ src/apis/scenariofieldconfig/index.ts
@@ -1,5 +1,8 @@
+import './delete'
 import './get'
 import './get-by-org-id'
 import './get-by-project-id'
 import './post'
 import './put'
+import './put-scenario-fields'
+import './put-scenario-info'
