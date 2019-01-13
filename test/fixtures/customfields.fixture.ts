20af6f5c31637e1cc95bbcf0e852519f024a7dad
Fai <aicest@users.noreply.github.com>
2019-01-13 16:16:17 +0800

    feat(apis/customfield): 添加 getCustomField 接口
    - 新增 `getCustomField` 接口
    - 新增 `getLinkByCustomFieldId` 接口

---
+++ test/fixtures/customfields.fixture.ts
@@ -0,0 +1,20 @@
+export const customField = {
+  boundType: 'application',
+  pos: 0,
+  displayed: false,
+  _roleIds: [],
+  categoryIds: [],
+  description: '',
+  _id: '5b9ec0b08c39420001e2f51a',
+  name: 'p',
+  type: 'text',
+  _advancedCustomfieldId: null,
+  externalUrl: '',
+  choices: [],
+  boundToObjectType: '',
+  _organizationId: '5ab48cb599d4ad00010e06cc',
+  _creatorId: '569ee653f87e2d3e6ece85b8',
+  _appId: '5b3c381980d435006c231e88',
+  created: '2018-09-16T20:44:32.322Z',
+  updated: '2018-09-16T20:44:32.322Z'
+}
