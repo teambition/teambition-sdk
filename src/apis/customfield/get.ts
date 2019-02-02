7425bd36c74eccc0b09ad4cef3abab67e0071fa3
Fai <aicest@users.noreply.github.com>
2019-02-02 19:05:45 +0800

    fixup! feat(schemas/CustomField): 定义“加锁”相关属性

--- src/apis/customfield/get.ts
+++ src/apis/customfield/get.ts
@@ -45,6 +45,13 @@ export function getCustomField(
       creator: ['_id', 'name', 'avatarUrl'],
       locker: ['_id', 'name', 'avatarUrl']
     },
+    // 使用 `getScenarioFieldConfigs` 拿到的 CustomField 数据已经满足各种业务场景的。
+    // 但这个 CustomField 会缺少 `projects` 数据。
+    // 但这个 `projects` 又不是 CustomField 自身属性，所以 `getScenarioFieldConfigs` 给的数据是合理的。
+
+    // 所以 `getScenarioFieldConfigs` 给的 CustomField 数据一旦通过 `options.request` 进入 RDB 缓存，
+    // 那么这里的 `getCustomField` 返回数据就始终缺少 `projects` 数据。
+    // 所以通过 `options.withProjects = required + padding` 来解决。
     required: options.withProjects ? ['projects'] : void 0,
     padding: () => this.fetch.getCustomField(customFieldId)
   })
