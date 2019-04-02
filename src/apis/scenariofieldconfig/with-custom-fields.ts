2e407ba99d4bc9e43788ff231e33d46cec1fc239
Fai <aicest@users.noreply.github.com>
2019-04-02 14:25:13 +0800

    fixup! fix(apis/scenariofieldconfig): 添加 forcePaddingCustomField 参数

--- src/apis/scenariofieldconfig/with-custom-fields.ts
+++ src/apis/scenariofieldconfig/with-custom-fields.ts
@@ -76,6 +76,15 @@ const normalizeScenarioField = (sdk: SDK, options: WithCustomFieldsOptions) => (
   )
 }
 
+/**
+ * 给 ScenarioField 填充 CustomField 数据
+ *
+ * 后端的 getScenarioFieldConfigs/getOrgScenarioFieldConfigs 接口支持 withCustomfields 参数，
+ * 当 withCustomfields=true 时，会把 CustomField 数据一并返回，挂在 scenarioField.customfield 字段上；
+ * 但后端没有处理 ScenarioFieldConfig 消息推送，推送数据里还是缺少 CustomField 数据，
+ * 不过后端会保证推送 CustomFieldLink 变化，因此可以依赖 scenarioField.customfield + CustomFieldLink 来解决问题，
+ * 这块逻辑相当于将 ScenarioFieldConfig 跟 CustomField 和 CustomFieldLink 进行关联。
+ */
 const getCustomField = (
   sdk: SDK,
   scenarioField: CustomScenarioFieldSchema,
@@ -85,11 +94,13 @@ const getCustomField = (
   const customFieldId = scenarioField._customfieldId
 
   // 自带 CustomField 数据
+  // 1. 当 withCustomfields=true 时，请求接口（getScenarioFieldConfigs/getOrgScenarioFieldConfigs）会返回 CustomField 数据，除非被删
+  // 2. 但 ScenarioFieldConfig 推送数据里，仍是缺失 CustomField 数据
   const customField$ = scenarioField.customfield
     ? Observable.of(scenarioField.customfield)
     : void 0
 
-  // [fallback] 缺少 CustomField 数据，当 forcePaddingCustomField=true 那么发送请求进行获取
+  // 缺少 CustomField 数据，当 forcePaddingCustomField=true 那么发送请求进行获取
   const customFieldPadding$ =
     options.forcePaddingCustomField &&
     // 从企业里请求 CustomField 数据
@@ -103,7 +114,9 @@ const getCustomField = (
       })
     )
 
-  // [fallback] 缺少 CustomField 数据，将 CustomFieldLink 当做 CustomField 使用
+  // 由于 ScenarioFieldConfig 推送数据里不包含 CustomField 变化，因此缺少 CustomField 数据，
+  // 但后端会保证推送 CustomFieldLink 变化，所以使用 CustomFieldLink 做为 CustomField 使用，
+  // 进而保证 scenarioField.customfield 准确性
   const customFieldLink$ = sdk
     .getLinkByCustomFieldId(customFieldId, { request: empty$ })
     .changes()
