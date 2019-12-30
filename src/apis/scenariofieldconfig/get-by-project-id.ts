e275f000f5214b38d2d04a88f28f0ae65f581001
Hopsken <shaowei5@icloud.com>
2019-12-30 16:33:42 +0800

    Revert "feat(TaskflowStatus): getScenarioFieldConfigs 的 assocFields 添加 hasTaskflowEngineConfig 字段"
    This reverts commit ae1ae1eb733e91757644f23af7332edb92b10c5e.

--- src/apis/scenariofieldconfig/get-by-project-id.ts
+++ src/apis/scenariofieldconfig/get-by-project-id.ts
@@ -117,7 +117,6 @@ export function getScenarioFieldConfigs(
               'name',
               'kind',
               'rejectStatusIds',
-              'hasTaskflowEngineConfig',
               'pos'
             ]
           }
