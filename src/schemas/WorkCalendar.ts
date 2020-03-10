735b7a2ca593b0b49c2e505af5014f15d409ef9b
Hutchins <418037025@qq.com>
2020-03-10 15:16:03 +0800

    feat: add workCalendar

---
+++ src/schemas/WorkCalendar.ts
@@ -0,0 +1,48 @@
+import { OrganizationId, WorkCalendarId } from 'teambition-types'
+import { RDBType, SchemaDef } from '../db'
+import { schemaColl } from './schemas'
+
+export interface WorkCalendarRule {
+  rule?: string
+  time: string[]
+  type: 'holiday' | 'workday'
+  start?: string
+  until?: string
+}
+
+export interface WorkCalendarSchema {
+  _id: WorkCalendarId
+  defaults: WorkCalendarRule[]
+  personalizations: WorkCalendarRule[]
+  isDeleted: boolean
+  created: string
+  updated: string
+  _organizationId: OrganizationId
+}
+
+const schema: SchemaDef<WorkCalendarSchema> = {
+  _id: {
+    type: RDBType.STRING,
+    primaryKey: true
+  },
+  defaults: {
+    type: RDBType.OBJECT
+  },
+  personalizations: {
+    type: RDBType.OBJECT
+  },
+  isDeleted: {
+    type: RDBType.BOOLEAN
+  },
+  created: {
+    type: RDBType.DATE_TIME
+  },
+  updated: {
+    type: RDBType.DATE_TIME
+  },
+  _organizationId: {
+    type: RDBType.STRING
+  }
+}
+
+schemaColl.add({ name: 'WorkCalendar', schema })
