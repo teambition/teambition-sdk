3d2fb449d0a2429e0cb73b48dd230fbdeaa0aeb2
靳昌 <chang@teambition.com>
2020-04-02 13:49:55 +0800

    feat(schemas/KanbanConfig): 增加 KanbanConfig 和相关接口

---
+++ src/schemas/KanbanConfig.ts
@@ -0,0 +1,46 @@
+import {
+  KanbanConfigId, ScenarioFieldConfigId, ProjectId
+} from 'teambition-types'
+
+import { RDBType, SchemaDef } from '../db'
+import { schemaColl } from './schemas'
+
+export enum KanbanConfigDisplayedField {
+  updated = 'updated',
+  creator = 'creator',
+  created = 'created',
+  accomplished = 'accomplished',
+}
+
+export interface KanbanConfigSchema {
+  _id: KanbanConfigId
+  _projectId: ProjectId
+  _scenariofieldconfigId: ScenarioFieldConfigId
+  created: string
+  displayedFields: ReadonlyArray<KanbanConfigDisplayedField>
+  updated: string
+}
+
+const schema: SchemaDef<KanbanConfigSchema> = {
+  _id: {
+    type: RDBType.STRING,
+    primaryKey: true,
+  },
+  _projectId: {
+    type: RDBType.STRING,
+  },
+  _scenariofieldconfigId: {
+    type: RDBType.STRING,
+  },
+  created: {
+    type: RDBType.DATE_TIME,
+  },
+  displayedFields: {
+    type: RDBType.OBJECT
+  },
+  updated: {
+    type: RDBType.DATE_TIME,
+  },
+}
+
+schemaColl.add({ name: 'KanbanConfig', schema })
