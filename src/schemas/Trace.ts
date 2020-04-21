b046b5961ec1fc1aa0bb10cd52e2659798591fc3
Fai <aicest@users.noreply.github.com>
2020-04-21 14:21:13 +0800

    feat(schemas): 新增 Trace 类型

---
+++ src/schemas/Trace.ts
@@ -0,0 +1,63 @@
+import { TraceId, DetailObjectId, DetailObjectType, UserSnippet } from 'teambition-types'
+import { SchemaDef, RDBType, Relationship } from '../db'
+import { schemaColl } from './schemas'
+import { FileSchema } from './File'
+
+export interface TraceSchema {
+  _boundToObjectId: DetailObjectId
+  _creatorId: TraceId
+  _id: TraceId
+  attachments: FileSchema[]
+  boundToObjectType: DetailObjectType
+  content: string
+  created: string
+  creator: UserSnippet
+  status: number
+  title: string
+  updated: string
+}
+
+const schema: SchemaDef<TraceSchema> = {
+  _boundToObjectId: {
+    type: RDBType.STRING
+  },
+  _creatorId: {
+    type: RDBType.STRING
+  },
+  _id: {
+    type: RDBType.STRING,
+    primaryKey: true
+  },
+  attachments: {
+    type: RDBType.OBJECT,
+  },
+  boundToObjectType: {
+    type: RDBType.STRING,
+  },
+  content: {
+    type: RDBType.STRING,
+  },
+  created: {
+    type: RDBType.STRING,
+  },
+  creator: {
+    type: Relationship.oneToOne,
+    virtual: {
+      name: 'User',
+      where: (userTable: any) => ({
+        _creatorId: userTable._id
+      })
+    }
+  },
+  status: {
+    type: RDBType.NUMBER,
+  },
+  title: {
+    type: RDBType.STRING,
+  },
+  updated: {
+    type: RDBType.STRING,
+  },
+}
+
+schemaColl.add({ schema, name: 'Trace' })
