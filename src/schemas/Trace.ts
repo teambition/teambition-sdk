e169eaeea6119a2f100667a4ddbc93c562aab06e
Fai <aicest@users.noreply.github.com>
2020-05-19 20:33:16 +0800

    feat(schemas/Trace): 补充 obectType、reminderIds、mentionIds 字段

--- src/schemas/Trace.ts
+++ src/schemas/Trace.ts
@@ -7,12 +7,17 @@ export interface TraceSchema {
   _boundToObjectId: DetailObjectId
   _creatorId: UserId
   _id: TraceId
+  activityCount: number
   attachments: FileSchema[]
   boundToObjectType: DetailObjectType
   content: string
   created: string
   creator: UserSnippet
+  likeCount: number
+  mentionIds: UserId[]
+  objectType: 'trace'
   raw: string
+  reminderIds: UserId[]
   renderMode: 'html'
   status: number
   title: string
@@ -29,6 +34,9 @@ const schema: SchemaDef<TraceSchema> = {
   _id: {
     type: RDBType.STRING,
     primaryKey: true
+  },
+  activityCount: {
+    type: RDBType.NUMBER,
   },
   attachments: {
     type: RDBType.OBJECT,
@@ -51,8 +59,20 @@ const schema: SchemaDef<TraceSchema> = {
       })
     }
   },
+  likeCount: {
+    type: RDBType.NUMBER,
+  },
+  mentionIds: {
+    type: RDBType.OBJECT,
+  },
+  objectType: {
+    type: RDBType.STRING,
+  },
   raw: {
     type: RDBType.STRING,
+  },
+  reminderIds: {
+    type: RDBType.OBJECT,
   },
   renderMode: {
     type: RDBType.STRING,
